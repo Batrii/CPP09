1d976e042fba529ebd0f4693009d3a3ef5cd7ae6
Batrii <dhj.nafiai@gmail.com>
2026-04-04 18:33:16 +0100

    fix the time taken calculation

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -20,15 +20,15 @@ class PmergeMe{
         // vector sort part
         void make_pairs(std::vector<int> &arr, std::vector<std::pair<int, int> > &pairs, int &straggler, bool &has_straggler);
         void sort_pairs(std::vector<std::pair<int, int> > &pairs);
-        void ford_johnson_sort(std::vector<int> &arr);
         void insert_smaller_into_bigger(std::vector<int> &bigger, std::vector<int> &smaller, int &leftover, bool has_leftover);
+        void ford_johnson_sort(std::vector<int> &arr);
         void print_array(const std::vector<int> &arr);
 
         // deque sort part
         void make_pairs(std::deque<int> &arr, std::deque<std::pair<int, int> > &pairs, int &straggler, bool &has_straggler);
         void sort_pairs(std::deque<std::pair<int, int> > &pairs);
-        void ford_johnson_sort(std::deque<int> &arr);
         void insert_smaller_into_bigger(std::deque<int> &bigger, std::deque<int> &smaller, int &leftover, bool has_leftover);
+        void ford_johnson_sort(std::deque<int> &arr);
         void print_array(const std::deque<int> &arr);
 };
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -39,16 +39,16 @@ int main(int argc, char **argv)
     if (put_args_in_vector_deque(argc, argv, numbers, numbers_deque) != 0)
         return 1;
     clock_t start, end;
-    start = clock();
     PmergeMe pmergeMe(numbers);
-
-
+    
+    
     std::cout << "Before: ";
     pmergeMe.print_array(numbers); 
+    start = clock();
     pmergeMe.ford_johnson_sort(numbers);
+    end = clock();
     std::cout << "After: ";
     pmergeMe.print_array(numbers);
-    end = clock();
     // the result in us
     double time_taken = static_cast<double>(end - start) * 1000000 / CLOCKS_PER_SEC;
     std::cout << "Time to process a range of " << numbers.size() << " elements with std::vector : " << time_taken << " us" << std::endl;
